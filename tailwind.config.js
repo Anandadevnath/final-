fe7c49674a1e659fa3f4ea10c5e3449c5b428c03
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-02-01 17:53:38 +0600

    new code

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'hclr': '#FFF9F4',
+        'bike-primary-bg': 'rgba(231,111,81,0.1)',
+        'imp': '#f9f9f9',
+        'btn-clr': '#FFDC26',
+        'card-bg': '#F9DF8A'
+
+      }
+
+    }
+
+  }
+}
